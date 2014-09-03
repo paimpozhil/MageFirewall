ce93b964a703f047fd915775c42607935fc10b04
imvrsuresh <suresh@blaze.ws>
2014-09-03 17:43:31 +0100

    deleted old namespace files

--- app/code/local/Mage/Wall/Helper/Data.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Mage_Wall_Helper_Data extends Mage_Core_Helper_Abstract
-{	
-}
-?>
--- app/code/local/Mage/Wall/Model/Mysql4/Version.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class Mage_Wall_Model_Mysql4_Version extends Mage_Core_Model_Mysql4_Abstract
-    {
-        public function _construct()
-        {   
-            $this->_init('wall/version', 'version_id');
-        }
-    }
